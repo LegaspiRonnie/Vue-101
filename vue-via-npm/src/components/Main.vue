a1e7a6cbe56a93a40721f64f533bdbc195a7fb0c
Ronnie Legaspi <ronnielegaspi98@gmail.com>
2026-07-24 01:42:16 +0800

    for loop

--- vue-via-npm/src/components/Main.vue
+++ vue-via-npm/src/components/Main.vue
@@ -1,39 +1,36 @@
 <script setup lang="ts">
   import {ref} from 'vue'
-  const h2s = ref([
-    "first",
-    "second",
-    "third"
-  ]);
-  const paragraphs = ref([
-    "P1",
-    "P2",
-    "P3"
-  ])
-  const hilights = ref([
-    "1",
-    "2",
-    "3"
-  ])
-
-  const spanClass = ref("hilight");
+  const titleClass = ref("title");
+  const descriptionClass = ref("description");
+  const pathToImages = ref("/images/");
+  
+  const posts = ref([{
+    image: `${pathToImages.value}image1.png`,
+    title: "This is a title 1",
+    description: "This is a description 1"
+  },
+  {
+    image: `${pathToImages.value}image2.png`,
+    title: "This is a title 2",
+    description : "This is a description 2"
+  }, 
+  {
+    image: `${pathToImages.value}image3.png`,
+    title: "This is a title 3",
+    description: "This is a description 3"
+  }])
 </script> 
 
 <template>
     
   <main>
-      <section>
-        <h2>{{h2s[0]}} <span :class="spanClass"> {{ hilights[0] }}</span></h2>
-        <p>{{ paragraphs[0] }}</p>
+    
+      <section v-for="post in posts">
+        <img :src=" post.image " alt="error loading image" />
+        <h2 :class="titleClass">{{ post.title }}</h2>
+        <p :class="descriptionClass">{{ post.description }}</p>
       </section>
-      <section>
-        <h2>{{h2s[1]}}<span :class="spanClass"> {{ hilights[1] }} </span></h2>
-        <p>{{ paragraphs[1] }}</p>
-      </section>
-      <section>
-        <h2>{{h2s[2]}}<span :class="spanClass"> {{ hilights[2] }}</span></h2>
-        <p>{{ paragraphs[2] }} </p>
-      </section>
+      
      
   </main>
 </template>
