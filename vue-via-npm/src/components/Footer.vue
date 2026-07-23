dca5b68a7dd78c05b182ce7b1acf496a5d931b93
Ronnie Legaspi <ronnielegaspi98@gmail.com>
2026-07-24 01:05:15 +0800

    test

--- vue-via-npm/src/components/Footer.vue
+++ vue-via-npm/src/components/Footer.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
 import {ref} from 'vue'
   const currentYear = new Date().getFullYear();
+  const href = ref("https://ronnie.dev");
+  const target = ref("_blank");
+
+  href.value = "https://ronnie.dev";
+  target.value = "_blank";
 </script>
 
 <template>
     <footer>
       <p>
-        &copy; {{ currentYear }}
+        <p>
+          Learn more about me at 
+          <a :href :target="target">
+            ronnie.dev
+          </a> &copy; {{ currentYear }}
+
+        </p>
       </p>
   </footer>
 </template>
--- vue-via-npm/src/components/Main.vue
+++ vue-via-npm/src/components/Main.vue
@@ -1,14 +1,40 @@
 <script setup lang="ts">
   import {ref} from 'vue'
-  const name = ref("Ronnie")
-  const paragraph = ref("Im about to learn Vue.js")
+  const h2s = ref([
+    "first",
+    "second",
+    "third"
+  ]);
+  const paragraphs = ref([
+    "P1",
+    "P2",
+    "P3"
+  ])
+  const hilights = ref([
+    "1",
+    "2",
+    "3"
+  ])
+
+  const spanClass = ref("hilight");
 </script> 
 
 <template>
     
   <main>
-      <h1>Hello {{name}}</h1>
-      <p>{{  paragraph  }}</p>
+      <section>
+        <h2>{{h2s[0]}} <span :class="spanClass"> {{ hilights[0] }}</span></h2>
+        <p>{{ paragraphs[0] }}</p>
+      </section>
+      <section>
+        <h2>{{h2s[1]}}<span :class="spanClass"> {{ hilights[1] }} </span></h2>
+        <p>{{ paragraphs[1] }}</p>
+      </section>
+      <section>
+        <h2>{{h2s[2]}}<span :class="spanClass"> {{ hilights[2] }}</span></h2>
+        <p>{{ paragraphs[2] }} </p>
+      </section>
+     
   </main>
 </template>
 
